3c3b8d5c76c55c97bef83c7792d5f5c00ddb4a37
Navojith <navojith22@gmail.com>
2023-03-13 19:55:49 +0530

    edit review

---
+++ app/src/main/java/com/example/healingwords/EditReview.kt
@@ -0,0 +1,11 @@
+package com.example.healingwords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditReview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_review)
+    }
+}
