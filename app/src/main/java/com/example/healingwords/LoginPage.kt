042967d6975464615d39c2eda620a908bca97cbb
ch4mi2 <chamithu4@gmail.com>
2023-04-05 16:03:59 +0530

    login an firebase

--- app/src/main/java/com/example/healingwords/LoginPage.kt
+++ app/src/main/java/com/example/healingwords/LoginPage.kt
@@ -1,11 +1,82 @@
 package com.example.healingwords
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginPage : AppCompatActivity() {
+
+    private lateinit var loginEmailText: EditText
+    private lateinit var loginPasswordText: EditText
+    private lateinit var submitLoginBtn: Button
+    private lateinit var loginRegBtn: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var loginProgress: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        loginEmailText = findViewById(R.id.editEmailLogin)
+        loginPasswordText = findViewById(R.id.editPasswordLogin)
+        submitLoginBtn = findViewById(R.id.submitLoginBtn)
+        loginRegBtn = findViewById(R.id.loginRegBtn)
+        loginProgress = findViewById(R.id.loginProgress)
+
+        submitLoginBtn.setOnClickListener {
+
+
+            val loginEmail = loginEmailText.text.toString()
+            val loginPass = loginPasswordText.text.toString()
+
+            if (loginEmail.isNotEmpty() && loginPass.isNotEmpty()) {
+                loginProgress.visibility = View.VISIBLE
+
+                mAuth.signInWithEmailAndPassword(loginEmail, loginPass)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+
+                            sendToMain()
+                        } else {
+                            val errorMessage = task.exception?.message
+                            Toast.makeText(this@LoginPage, "Error: $errorMessage", Toast.LENGTH_LONG).show()
+
+
+                        }
+
+                        loginProgress.visibility = View.INVISIBLE
+                    }
+            }
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = mAuth.currentUser
+
+        if(currentUser != null){
+
+            sendToMain()
+
+        }
+
+    }
+
+    private fun sendToMain() {
+        val mainIntent = Intent(this, MainActivity::class.java)
+        startActivity(mainIntent)
+        finish()
+    }
+
 }
--- app/src/main/java/com/example/healingwords/MainActivity.kt
+++ app/src/main/java/com/example/healingwords/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.healingwords
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainToolBar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_reviews)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser;
+        if(currentUser == null){
+            val loginIntent = Intent(this, LoginPage::class.java)
+            startActivity(loginIntent);
+            finish();
+        }
     }
 }
