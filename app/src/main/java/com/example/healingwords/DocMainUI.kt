ae61f7cbf92b6bd4fcb7e1378f03660064373ec7
Navojith <navojith22@gmail.com>
2023-04-21 13:34:12 +0530

    nav ffrag

--- app/src/main/java/com/example/healingwords/DocMainUI.kt
+++ app/src/main/java/com/example/healingwords/DocMainUI.kt
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import com.example.healingwords.databinding.ActivityDocMainUiBinding
+import com.example.healingwords.databinding.ActivityRegisterDoctorPageBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class DocMainUI : AppCompatActivity() {
     private lateinit var mainToolBar: Toolbar
-
+    private lateinit var binding: ActivityDocMainUiBinding
     private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doc_main_ui)
+        binding = ActivityDocMainUiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         mAuth = FirebaseAuth.getInstance()
 
         mainToolBar = findViewById(R.id.mainToolBar)
@@ -22,6 +26,43 @@ class DocMainUI : AppCompatActivity() {
 
         supportActionBar?.setTitle("Healing Words")
 
+        //ui elements binding
+        val imgHome = binding.imgHomeDoc
+        val imgReviews = binding.imgReviewsDoc
+        val imgBlogs = binding.imgBlogsDoc
+        val imgProfile = binding.imgDoc
+
+        //fragments
+        val fragmentHome = ""
+        val fragmentReviews = ShowAllReviews()
+        val fragmentBlogs = ""
+        val fragmentDocProfile = DocProfile()
+
+        //navigation profile page
+        imgProfile.setOnClickListener{
+            imgHome.setImageResource(R.drawable.unselected_home)
+            imgBlogs.setImageResource(R.drawable.unselected_blog)
+            imgReviews.setImageResource(R.drawable.review_unselected)
+            imgProfile.setImageResource(R.drawable.selected_user)
+
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.DocMainUIFragmentContainerView.id, fragmentDocProfile)
+                commit()
+            }
+        }
+
+        //navigation reviews page
+        imgReviews.setOnClickListener{
+            imgHome.setImageResource(R.drawable.unselected_home)
+            imgBlogs.setImageResource(R.drawable.unselected_blog)
+            imgReviews.setImageResource(R.drawable.review_selected)
+            imgProfile.setImageResource(R.drawable.unselected_user)
+
+            supportFragmentManager.beginTransaction().apply {
+                replace(binding.DocMainUIFragmentContainerView.id, fragmentReviews)
+                commit()
+            }
+        }
 
     }
 
