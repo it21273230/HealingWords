c5a2899b55de4ca20ff885cc5a5af961996dc718
Navojith <navojith22@gmail.com>
2023-04-29 12:28:33 +0530

    edit review nav

--- app/src/main/java/com/example/healingwords/adapters/ReviewListAdapter.kt
+++ app/src/main/java/com/example/healingwords/adapters/ReviewListAdapter.kt
@@ -1,19 +1,20 @@
 package com.example.healingwords.adapters
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
-import androidx.fragment.app.FragmentManager
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.healingwords.EditReview
 import com.example.healingwords.R
 import com.example.healingwords.models.Review
 
 class ReviewListAdapter(private val reviewList: ArrayList<Review>) : RecyclerView.Adapter<ReviewListAdapter.ReviewListViewHolder>() {
-    lateinit var editBtn: ImageView
     private lateinit var mListener: OnItemClickListener
 
     interface OnItemClickListener {
@@ -37,19 +38,25 @@ class ReviewListAdapter(private val reviewList: ArrayList<Review>) : RecyclerVie
         holder.rate.rating = currentItem.noOfStars!!.toFloat()
         holder.user.text = "Anonymous"
         holder.ratingText.text = currentItem.description.toString()
-        holder.editBtn
+        holder.editBtn.setOnClickListener {
+            val intent = Intent(holder.contexts, EditReview::class.java)
+            intent.putExtra("reviewId", currentItem.reviewId)
+            holder.contexts.startActivity(intent)
+
+        }
     }
+
 
     override fun getItemCount(): Int {
         return reviewList.size
     }
 
     class ReviewListViewHolder(itemView: View, listener: OnItemClickListener) : RecyclerView.ViewHolder(itemView) {
+        val contexts: Context = itemView.context
         val user : TextView = itemView.findViewById(R.id.user)
         val ratingText: TextView = itemView.findViewById(R.id.rating2_text2)
         val rate: RatingBar = itemView.findViewById(R.id.rating)
         val editBtn : ImageView = itemView.findViewById(R.id.edit3)
-
         init {
             itemView.setOnClickListener{
                 listener.onItemClick(adapterPosition)
