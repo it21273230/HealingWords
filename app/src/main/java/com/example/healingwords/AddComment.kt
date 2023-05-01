2511340df90429fd1d4e64843ee1e0e350d0a9db
it21273230 <gamindudammith@gmail.com>
2023-05-01 17:41:34 +0530

    add comment

--- app/src/main/java/com/example/healingwords/AddComment.kt
+++ app/src/main/java/com/example/healingwords/AddComment.kt
@@ -1,11 +1,49 @@
 package com.example.healingwords
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.healingwords.databinding.ActivityAddCommentBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class AddComment : AppCompatActivity() {
+
+    private lateinit var postId: String
+    private lateinit var userId: String
+    private lateinit var binding: ActivityAddCommentBinding
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_comment)
+        binding = ActivityAddCommentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        postId = intent.getStringExtra("postId") ?: ""
+        userId = intent.getStringExtra("userId") ?: ""
+
+        binding.Add.setOnClickListener{
+
+            val comment = binding.commentBody.text.toString()
+
+
+            database = FirebaseDatabase.getInstance().getReference("comments")
+            val Comment = Comment(comment, userId, postId )
+
+            database.child(comment).setValue(Comment).addOnSuccessListener {
+
+                binding.commentBody.text.clear()
+
+                Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show()
+
+            }.addOnFailureListener{
+                Toast.makeText(this, "fail to add", Toast.LENGTH_SHORT).show()
+
+            }
+            val intent = Intent(this, ViewComments::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/com/example/healingwords/ViewComments.kt
+++ app/src/main/java/com/example/healingwords/ViewComments.kt
@@ -1,12 +1,17 @@
 package com.example.healingwords
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ViewComments : AppCompatActivity() {
 
     private lateinit var postId: String
     private lateinit var userId: String
+
+    private lateinit var AddCommentbtn: FloatingActionButton
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +21,19 @@ class ViewComments : AppCompatActivity() {
 
         postId = intent.getStringExtra("postId") ?: ""
         userId = intent.getStringExtra("userId") ?: ""
+
+        AddCommentbtn = findViewById(R.id.AddCommentbtn)
+
+
+
+        AddCommentbtn.setOnClickListener{
+            Log.d("postId", postId)
+
+            val intent = Intent(this, AddComment::class.java)
+            intent.putExtra("postId", postId)
+            intent.putExtra("userId", userId)
+            startActivity(intent)
+        }
 
 
     }
