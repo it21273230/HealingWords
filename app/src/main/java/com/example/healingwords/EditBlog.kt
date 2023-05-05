bc8012639da6c2a4e9ba290b81fbbb6e9adb093d
IT21216046 <it21216046@my.sliit.lk>
2023-05-05 11:42:07 +0530

    update blogs

--- app/src/main/java/com/example/healingwords/EditBlog.kt
+++ app/src/main/java/com/example/healingwords/EditBlog.kt
@@ -2,6 +2,9 @@ package com.example.healingwords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.healingwords.databinding.ActivityEditBlogBinding
 import com.google.firebase.database.DatabaseReference
@@ -13,18 +16,44 @@ class EditBlog : AppCompatActivity() {
     private lateinit var database: DatabaseReference
     private lateinit var blogid : String
     private lateinit var docId : String
+    private lateinit var title : EditText
+    private lateinit var desc : EditText
+    private lateinit var submitEditBtn : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityEditBlogBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityEditBlogBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_edit_blog)
 
-        binding.submitEditBtn.setOnClickListener {
+        database = FirebaseDatabase.getInstance().reference
+
+        title = findViewById(R.id.editBlogTitle)
+        desc = findViewById(R.id.editBlogDesc)
+        submitEditBtn = findViewById(R.id.submitEditBtn)
+
+        blogid = intent.getStringExtra("blogId")?:""
+
+        database.child("Blogs").child(blogid).get().addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                val dataSnapshot = task.result
+                if (dataSnapshot.exists())
+                {
+                    val blogTitle = dataSnapshot.child("title").value.toString()
+                    val blogDesc = dataSnapshot.child("desc").value.toString()
+
+                    title.setText(blogTitle)
+                    desc.setText(blogDesc)
+
+                }
+            }
+        }
+
+        submitEditBtn.setOnClickListener {
 
 
-            val title = binding.editBlogTitle.text.toString()
-            val des = binding.editBlogDesc.text.toString()
+            val title = title.text.toString()
+            val des = desc.text.toString()
 
             updateBlog(title,des)
 
@@ -38,7 +67,8 @@ class EditBlog : AppCompatActivity() {
     }
 
     private fun updateBlog(title:String,des:String){
-        database = FirebaseDatabase.getInstance().getReference("Blogs")
+        Log.d("title",title)
+
 
 
 
@@ -47,10 +77,9 @@ class EditBlog : AppCompatActivity() {
             "desc" to des
         )
 
-        database.child(title).updateChildren(blog).addOnSuccessListener {
+        database.child("Blogs").child(blogid).updateChildren(blog).addOnSuccessListener {
 
-            binding.editBlogTitle.text.clear()
-            binding.editBlogDesc.text.clear()
+
             Toast.makeText(this,"Updated",Toast.LENGTH_SHORT).show()
         }.addOnFailureListener{
             Toast.makeText(this,"Not Updated",Toast.LENGTH_SHORT).show()
