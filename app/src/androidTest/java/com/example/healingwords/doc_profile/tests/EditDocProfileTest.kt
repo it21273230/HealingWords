a16414191ff39407fe5a73efab25fcbd732124ad
Navojith <navojith22@gmail.com>
2023-05-05 23:36:17 +0530

    testing aye nam baa

---
+++ app/src/androidTest/java/com/example/healingwords/doc_profile/tests/EditDocProfileTest.kt
@@ -0,0 +1,77 @@
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.*
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import org.hamcrest.Matchers.*
+import org.junit.*
+import org.junit.runner.RunWith
+
+import org.junit.Rule
+import org.junit.Test
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.example.healingwords.*
+
+
+@RunWith(AndroidJUnit4::class)
+class EditDocProfileTest {
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(EditDocProfile::class.java)
+
+    // Add this rule to enable intent verification
+    @get:Rule
+    var intentsTestRule = IntentsTestRule(EditDocProfile::class.java)
+
+    private lateinit var activityScenario: ActivityScenario<EditDocProfile>
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+
+    @Before
+    fun setUp() {
+        activityScenario = activityScenarioRule.scenario
+        // Initialize Firebase and database references
+
+    }
+
+    @Test
+    fun testLoginSuccess() {
+        // Perform login
+
+        onView(withId(R.id.btnEditDocProfileConfirm)).perform(click())
+
+
+        // Wait for the login to complete and the MainActivity to start
+        intending(hasComponent(DocMainUI::class.java.name)).respondWith(
+            Instrumentation.ActivityResult(Activity.RESULT_OK, null)
+        )
+
+        onView(withId(R.id.docBottomNav)).check(matches(isDisplayed()))
+    }
+
+
+
+    @After
+    fun tearDown() {
+        activityScenario.close()
+    }
+}
+
--- app/src/test/java/com/example/healingwords/RatingCalculationTest.kt
+++ app/src/test/java/com/example/healingwords/RatingCalculationTest.kt
@@ -1,6 +1,7 @@
 package com.example.healingwords
 
 import com.example.healingwords.ratingCalculation.RatingCalc
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -10,8 +11,34 @@ class RatingCalculationTest {
 
     var reviewDbRef = FirebaseDatabase.getInstance().getReference("Reviews")
 
+    // correct uid
     @Test
-    fun testCal() {
-        assertEquals(0, rating.calculate("No Id", reviewDbRef))
+    fun testCase1() {
+        assertEquals(4.5, rating.calculate("CK2dLXaVTBYzwMi1SrEW1KA2M8n1", reviewDbRef))
+    }
+
+    // invalid uid
+    @Test
+    fun testCase2() {
+        assertEquals(0, rating.calculate("invalid Doc id", reviewDbRef))
+    }
+
+    // empty string as uid
+    @Test
+    fun testCase3() {
+        assertEquals(0, rating.calculate("", reviewDbRef))
+    }
+
+    // valid different uid
+    @Test
+    fun testCase4() {
+        assertEquals(3.5, rating.calculate("sdKpaEW1KA2M8nKasYzwMi1Sr1", reviewDbRef))
+    }
+
+    // wrong database
+    @Test
+    fun testCase5() {
+        val userDB = FirebaseDatabase.getInstance().getReference("User")
+        assertEquals(0, rating.calculate("No Id", userDB))
     }
 }
